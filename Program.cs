3b8cda0d580a7d30465fb917ae2efb3e57a7bc47
mkfrederick <freder3@mail.nmc.edu>
2026-02-23 20:42:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace bookStoreRecord
+{
+    internal class Program
+    {
+        public record Bookstore(int ID, string Title, string Author, double Price);
+        static void Main(string[] args)
+        {
+            Bookstore b0 = new(0, "30-Second Einstein", "Brian Clegg", 9.99);
+            Console.WriteLine(b0);
+            Bookstore b1 = new(1, "The Hobbit", "J.R.R. Tolkien", 10.99);
+            Console.WriteLine(b1);
+            Bookstore b2 = new(2, "All The Wrong Questions: Who Could That Be at This Hour?", "Lemony Snicket", 7.00);
+            Console.WriteLine(b2);
+        }
+    }
+}
